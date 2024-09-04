d078ebdfbcd2abad6b92e9d6442639dd1abe5cb4
Rohith2226042 <rohithchitti2226@gmail.com>
2024-09-04 21:11:37 +0530

    Create aws_deploy.py

---
+++ src/aws_deploy.py
@@ -0,0 +1,8 @@
+import boto3
+
+def deploy_infrastructure():
+    print("Deploying AWS infrastructure using Python...")
+    # Add your Boto3 commands here
+
+if __name__ == "__main__":
+    deploy_infrastructure()
